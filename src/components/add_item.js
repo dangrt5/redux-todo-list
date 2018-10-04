5ed9ef02b1f5f150353d66ac6bd5f3c0828cdc71
dangrt5 <randy.dang05@gmail.com>
2018-10-03 18:07:23 -0700

    redux form component added to todo list

---
+++ src/components/add_item.js
@@ -0,0 +1,48 @@
+import React from "react";
+import {Field, reduxForm} from "redux-form";
+
+class AddItem extends React.Component {
+  renderInput(props) {
+    console.log("Render Input Arguments", props);
+    return (
+      <div className="row">
+        <div className="s12">
+          <label>Label Here</label>
+          <input {...props.input} type="text"/>
+        </div>
+      </div>
+    )
+  }
+
+  saveItem = (values) => {
+    console.log("Values", values);
+
+  }
+
+  render() {
+    console.log("Add Item Props", this.props);
+    const {handleSubmit} = this.props;
+    return (
+      <div>
+        <h1 className="center">Add Item</h1>
+        <div className="row">
+          <div className="col s8 offset-s2">
+            <form onSubmit={handleSubmit(this.saveItem)}>
+              <Field name="title" component={this.renderInput}/>
+              <Field name="details" component={this.renderInput}/>
+              <div className="row">
+                <div className="s12 right-align">
+                  <button className="btn yellow darken-4">Add Item</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default reduxForm({
+  form: "add-item"
+})(AddItem)
