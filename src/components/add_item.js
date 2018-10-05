51f5ac6718d036085cd32d265b6dd03061f42151
dangrt5 <randy.dang05@gmail.com>
2018-10-05 11:55:55 -0700

    added item to server complete

--- src/components/add_item.js
+++ src/components/add_item.js
@@ -1,22 +1,27 @@
 import React from "react";
 import {Field, reduxForm} from "redux-form";
+import {connect} from "react-redux";
+import {addListItem} from "../actions";
 
 class AddItem extends React.Component {
   renderInput(props) {
+    const {input, label, meta: {touched, error} } = props;
     console.log("Render Input Arguments", props);
     return (
       <div className="row">
         <div className="s12">
-          <label>Label Here</label>
-          <input {...props.input} type="text"/>
+          <label>{label}</label>
+          <input {...input} type="text" autoComplete="off"/>
+          <p className="red-text text-darken-2">{touched && error}</p>
         </div>
       </div>
     )
   }
 
-  saveItem = (values) => {
+  saveItem = async (values) => {
     console.log("Values", values);
-
+    await this.props.addListItem(values);
+    this.props.history.push('/');
   }
 
   render() {
@@ -28,8 +33,8 @@ class AddItem extends React.Component {
         <div className="row">
           <div className="col s8 offset-s2">
             <form onSubmit={handleSubmit(this.saveItem)}>
-              <Field name="title" component={this.renderInput}/>
-              <Field name="details" component={this.renderInput}/>
+              <Field name="title" component={this.renderInput} label="Title"/>
+              <Field name="details" component={this.renderInput} label="Details"/>
               <div className="row">
                 <div className="s12 right-align">
                   <button className="btn yellow darken-4">Add Item</button>
@@ -43,6 +48,25 @@ class AddItem extends React.Component {
   }
 }
 
-export default reduxForm({
-  form: "add-item"
+function validate(values) {
+    const {title, details} = values;
+    const errors = {};
+    if(!title) {
+      errors.title = "Please give your item a title";
+    }
+
+    if(!details) {
+      errors.details = "Please give your item some details";
+    }
+
+    return errors;
+}
+
+AddItem = reduxForm({
+  form: "add-item",
+  validate: validate
 })(AddItem)
+
+export default connect(null, {
+  addListItem: addListItem
+})(AddItem);
